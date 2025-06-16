cfc681e833a81924ec4e89b98277c517435752d4
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2025-06-16 08:08:00 +0000

    feat: Add instance subcommands + upgrade go-ovh

--- internal/cmd/cloud_instance.go
+++ internal/cmd/cloud_instance.go
@@ -26,5 +26,47 @@ func initInstanceCommand(cloudCmd *cobra.Command) {
 		Args:  cobra.ExactArgs(1),
 	})
 
+	instanceCmd.AddCommand(&cobra.Command{
+		Use:   "start <instance_id>",
+		Short: "Start the given instance",
+		Run:   cloud.StartInstance,
+		Args:  cobra.ExactArgs(1),
+	})
+
+	instanceCmd.AddCommand(&cobra.Command{
+		Use:   "stop <instance_id>",
+		Short: "Stop the given instance",
+		Run:   cloud.StopInstance,
+		Args:  cobra.ExactArgs(1),
+	})
+
+	instanceCmd.AddCommand(&cobra.Command{
+		Use:   "shelve <instance_id>",
+		Short: "Shelve the given instance",
+		Long: `The resources dedicated to the Public Cloud instance are released.
+The data of the local storage will be stored, the duration of the operation depends on the size of the local disk.
+The instance can be unshelved at any time. Meanwhile hourly instances will not be billed.
+The Snapshot Storage used to store the instance's data will be billed.`,
+		Run:  cloud.ShelveInstance,
+		Args: cobra.ExactArgs(1),
+	})
+
+	instanceCmd.AddCommand(&cobra.Command{
+		Use:   "unshelve <instance_id>",
+		Short: "Unshelve the given instance",
+		Long: `The resources dedicated to the Public Cloud instance are restored.
+The duration of the operation depends on the size of the local disk.
+Instance billing will get back to normal and the snapshot used to store the instance's data will be deleted.`,
+		Run:  cloud.UnshelveInstance,
+		Args: cobra.ExactArgs(1),
+	})
+
+	instanceCmd.AddCommand(&cobra.Command{
+		Use:   "resume <instance_id>",
+		Short: "Resume the given suspended instance",
+		Run:   cloud.ResumeInstance,
+		Args:  cobra.ExactArgs(1),
+	})
+
 	cloudCmd.AddCommand(instanceCmd)
 }
--- internal/services/cloud/cloud_instance.go
+++ internal/services/cloud/cloud_instance.go
@@ -3,10 +3,12 @@ package cloud
 import (
 	_ "embed"
 	"fmt"
+	"net/url"
 
 	"github.com/spf13/cobra"
 	"stash.ovh.net/api/ovh-cli/internal/display"
 	"stash.ovh.net/api/ovh-cli/internal/flags"
+	httpLib "stash.ovh.net/api/ovh-cli/internal/http"
 	"stash.ovh.net/api/ovh-cli/internal/services/common"
 )
 
@@ -33,4 +35,89 @@ func GetInstance(_ *cobra.Command, args []string) {
 		return
 	}
 	common.ManageObjectRequest(fmt.Sprintf("/cloud/project/%s/instance", projectID), args[0], cloudInstanceTemplate)
+}
+
+func StartInstance(_ *cobra.Command, args []string) {
+	projectID, err := getConfiguredCloudProject()
+	if err != nil {
+		display.ExitError(err.Error())
+		return
+	}
+
+	endpoint := fmt.Sprintf("/cloud/project/%s/instance/%s/start", projectID, url.PathEscape(args[0]))
+
+	if err := httpLib.Client.Post(endpoint, nil, nil); err != nil {
+		display.ExitError("error starting instance %s: %s\n", args[0], err)
+		return
+	}
+
+	fmt.Println("\n⚡️ Instance starting ...")
+}
+
+func StopInstance(_ *cobra.Command, args []string) {
+	projectID, err := getConfiguredCloudProject()
+	if err != nil {
+		display.ExitError(err.Error())
+		return
+	}
+
+	endpoint := fmt.Sprintf("/cloud/project/%s/instance/%s/stop", projectID, url.PathEscape(args[0]))
+
+	if err := httpLib.Client.Post(endpoint, nil, nil); err != nil {
+		display.ExitError("error stopping instance %s: %s\n", args[0], err)
+		return
+	}
+
+	fmt.Println("\n⚡️ Instance stopping ...")
+}
+
+func ShelveInstance(_ *cobra.Command, args []string) {
+	projectID, err := getConfiguredCloudProject()
+	if err != nil {
+		display.ExitError(err.Error())
+		return
+	}
+
+	endpoint := fmt.Sprintf("/cloud/project/%s/instance/%s/shelve", projectID, url.PathEscape(args[0]))
+
+	if err := httpLib.Client.Post(endpoint, nil, nil); err != nil {
+		display.ExitError("error shelving instance %s: %s\n", args[0], err)
+		return
+	}
+
+	fmt.Println("\n⚡️ Instance is being shelved ...")
+}
+
+func UnshelveInstance(_ *cobra.Command, args []string) {
+	projectID, err := getConfiguredCloudProject()
+	if err != nil {
+		display.ExitError(err.Error())
+		return
+	}
+
+	endpoint := fmt.Sprintf("/cloud/project/%s/instance/%s/unshelve", projectID, url.PathEscape(args[0]))
+
+	if err := httpLib.Client.Post(endpoint, nil, nil); err != nil {
+		display.ExitError("error unshelving instance %s: %s\n", args[0], err)
+		return
+	}
+
+	fmt.Println("\n⚡️ Instance is being unshelved ...")
+}
+
+func ResumeInstance(_ *cobra.Command, args []string) {
+	projectID, err := getConfiguredCloudProject()
+	if err != nil {
+		display.ExitError(err.Error())
+		return
+	}
+
+	endpoint := fmt.Sprintf("/cloud/project/%s/instance/%s/resume", projectID, url.PathEscape(args[0]))
+
+	if err := httpLib.Client.Post(endpoint, nil, nil); err != nil {
+		display.ExitError("error resuming instance %s: %s\n", args[0], err)
+		return
+	}
+
+	fmt.Println("\n⚡️ Instance is being resumed ...")
 }
